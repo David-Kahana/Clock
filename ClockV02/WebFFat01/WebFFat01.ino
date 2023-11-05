c76d8398340e557af13013d4ceebad2e5f5c0f17
David-Kahana <david.kahana@gmail.com>
2023-11-05 10:16:20 +0200

    internal FFat by async web server

---
+++ ClockV02/WebFFat01/WebFFat01.ino
@@ -0,0 +1,361 @@
+#include "FS.h"
+#include "FFat.h"
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebSrv.h>
+
+// This file should be compiled with 'Partition Scheme' (in Tools menu)
+// set to 'Default with ffat' if you have a 4MB ESP32 dev module or
+// set to '16M Fat' if you have a 16MB ESP32 dev module.
+
+// You only need to format FFat the first time you run a test
+#define FORMAT_FFAT false
+
+const String default_ssid = "Knet2.4";
+const String default_wifipassword = "manuwlan";
+const int default_webserverporthttp = 80;
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="UTF-8">
+</head>
+<body>
+  <p><h1>File Upload</h1></p>
+  <p>Free Storage: %FREESPIFFS% | Used Storage: %USEDSPIFFS% | Total Storage: %TOTALSPIFFS%</p>
+  <form method="POST" action="/upload" enctype="multipart/form-data"><input type="file" name="data"/><input type="submit" name="upload" value="Upload" title="Upload File"></form>
+  <p>After clicking upload it will take some time for the file to firstly upload and then be written to SPIFFS, there is no indicator that the upload began.  Please be patient.</p>
+  <p>Once uploaded the page will refresh and the newly uploaded file will appear in the file list.</p>
+  <p>If a file does not appear, it will be because the file was too big, or had unusual characters in the file name (like spaces).</p>
+  <p>You can see the progress of the upload by watching the serial output.</p>
+  <p>%FILELIST%</p>
+</body>
+</html>
+)rawliteral";
+
+String listFilesRoot(bool ishtml = false);
+
+// configuration structure
+struct Config {
+  String ssid;               // wifi ssid
+  String wifipassword;       // wifi password
+  int webserverporthttp;     // http port number for web admin
+};
+
+Config config;                        // configuration
+AsyncWebServer *server;               // initialise webserver
+
+
+void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
+    Serial.printf("Listing directory: %s\r\n", dirname);
+
+    File root = fs.open(dirname);
+    if(!root){
+        Serial.println("- failed to open directory");
+        return;
+    }
+    if(!root.isDirectory()){
+        Serial.println(" - not a directory");
+        return;
+    }
+
+    File file = root.openNextFile();
+    while(file){
+        if(file.isDirectory()){
+            Serial.print("  DIR : ");
+            Serial.println(file.name());
+            if(levels){
+                listDir(fs, file.path(), levels -1);
+            }
+        } else {
+            Serial.print("  FILE: ");
+            Serial.print(file.name());
+            Serial.print("\tSIZE: ");
+            Serial.println(file.size());
+        }
+        file = root.openNextFile();
+    }
+}
+
+void readFile(fs::FS &fs, const char * path){
+    Serial.printf("Reading file: %s\r\n", path);
+
+    File file = fs.open(path);
+    if(!file || file.isDirectory()){
+        Serial.println("- failed to open file for reading");
+        return;
+    }
+
+    Serial.println("- read from file:");
+    while(file.available()){
+        Serial.write(file.read());
+    }
+    file.close();
+}
+
+void writeFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Writing file: %s\r\n", path);
+
+    File file = fs.open(path, FILE_WRITE);
+    if(!file){
+        Serial.println("- failed to open file for writing");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("- file written");
+    } else {
+        Serial.println("- write failed");
+    }
+    file.close();
+}
+
+void appendFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Appending to file: %s\r\n", path);
+
+    File file = fs.open(path, FILE_APPEND);
+    if(!file){
+        Serial.println("- failed to open file for appending");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("- message appended");
+    } else {
+        Serial.println("- append failed");
+    }
+    file.close();
+}
+
+void renameFile(fs::FS &fs, const char * path1, const char * path2){
+    Serial.printf("Renaming file %s to %s\r\n", path1, path2);
+    if (fs.rename(path1, path2)) {
+        Serial.println("- file renamed");
+    } else {
+        Serial.println("- rename failed");
+    }
+}
+
+void deleteFile(fs::FS &fs, const char * path){
+    Serial.printf("Deleting file: %s\r\n", path);
+    if(fs.remove(path)){
+        Serial.println("- file deleted");
+    } else {
+        Serial.println("- delete failed");
+    }
+}
+
+void testFileIO(fs::FS &fs, const char * path){
+    Serial.printf("Testing file I/O with %s\r\n", path);
+
+    static uint8_t buf[512];
+    size_t len = 0;
+    File file = fs.open(path, FILE_WRITE);
+    if(!file){
+        Serial.println("- failed to open file for writing");
+        return;
+    }
+
+    size_t i;
+    Serial.print("- writing" );
+    uint32_t start = millis();
+    for(i=0; i<2048; i++){
+        if ((i & 0x001F) == 0x001F){
+          Serial.print(".");
+        }
+        file.write(buf, 512);
+    }
+    Serial.println("");
+    uint32_t end = millis() - start;
+    Serial.printf(" - %u bytes written in %u ms\r\n", 2048 * 512, end);
+    file.close();
+
+    file = fs.open(path);
+    start = millis();
+    end = start;
+    i = 0;
+    if(file && !file.isDirectory()){
+        len = file.size();
+        size_t flen = len;
+        start = millis();
+        Serial.print("- reading" );
+        while(len){
+            size_t toRead = len;
+            if(toRead > 512){
+                toRead = 512;
+            }
+            file.read(buf, toRead);
+            if ((i++ & 0x001F) == 0x001F){
+              Serial.print(".");
+            }
+            len -= toRead;
+        }
+        Serial.println("");
+        end = millis() - start;
+        Serial.printf("- %u bytes read in %u ms\r\n", flen, end);
+        file.close();
+    } else {
+        Serial.println("- failed to open file for reading");
+    }
+}
+
+void setup(){
+    Serial.begin(115200);
+    //Serial.setDebugOutput(true);
+    if (FORMAT_FFAT) FFat.format();
+    if(!FFat.begin()){
+        Serial.println("FFat Mount Failed");
+        return;
+    }
+
+    Serial.printf("Total space: %10u\n", FFat.totalBytes());
+    Serial.printf("Free space: %10u\n", FFat.freeBytes());
+    //listDir(FFat, "/", 0);
+    Serial.println("listFilesRoot: ");
+    listFilesRoot();
+    //writeFile(FFat, "/hello.txt", "Hello ");
+    //appendFile(FFat, "/hello.txt", "World!\r\n");
+    //readFile(FFat, "/hello.txt");
+    //renameFile(FFat, "/hello.txt", "/foo.txt");
+    //readFile(FFat, "/foo.txt");
+    //deleteFile(FFat, "/foo.txt");
+    //testFileIO(FFat, "/test.txt");
+    //Serial.printf("Free space: %10u\n", FFat.freeBytes());
+    //deleteFile(FFat, "/test.txt");
+    //Serial.println( "Test complete" );
+
+    Serial.println("\nLoading Configuration ...");
+    config.ssid = default_ssid;
+    config.wifipassword = default_wifipassword;
+    config.webserverporthttp = default_webserverporthttp;
+    Serial.print("\nConnecting to Wifi: ");
+    WiFi.begin(config.ssid.c_str(), config.wifipassword.c_str());
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+
+    Serial.println("\n\nNetwork Configuration:");
+    Serial.println("----------------------");
+    Serial.print("         SSID: "); Serial.println(WiFi.SSID());
+    Serial.print("  Wifi Status: "); Serial.println(WiFi.status());
+    Serial.print("Wifi Strength: "); Serial.print(WiFi.RSSI()); Serial.println(" dBm");
+    Serial.print("          MAC: "); Serial.println(WiFi.macAddress());
+    Serial.print("           IP: "); Serial.println(WiFi.localIP());
+    Serial.print("       Subnet: "); Serial.println(WiFi.subnetMask());
+    Serial.print("      Gateway: "); Serial.println(WiFi.gatewayIP());
+    Serial.print("        DNS 1: "); Serial.println(WiFi.dnsIP(0));
+    Serial.print("        DNS 2: "); Serial.println(WiFi.dnsIP(1));
+    Serial.print("        DNS 3: "); Serial.println(WiFi.dnsIP(2));
+
+    // configure web server
+    Serial.println("\nConfiguring Webserver ...");
+    server = new AsyncWebServer(config.webserverporthttp);
+    configureWebServer();
+
+    // startup web server
+    Serial.println("Starting Webserver ...");
+    server->begin();
+}
+
+void loop(){
+}
+
+void rebootESP(String message) {
+  Serial.print("Rebooting ESP32: "); Serial.println(message);
+  ESP.restart();
+}
+
+// list all of the files, if ishtml=true, return html rather than simple text
+String listFilesRoot(bool ishtml) {
+  String returnText = "";
+  Serial.println("Listing files stored on FFat");
+  File root = FFat.open("/");
+  File foundfile = root.openNextFile();
+  if (ishtml) {
+    returnText += "<table><tr><th align='left'>Name</th><th align='left'>Size</th></tr>";
+  }
+  while (foundfile) {
+    if (ishtml) {
+      returnText += "<tr align='left'><td>" + String(foundfile.name()) + "</td><td>" + humanReadableSize(foundfile.size()) + "</td></tr>";
+    } else {
+      returnText += "File: " + String(foundfile.name()) + "\n";
+    }
+    foundfile = root.openNextFile();
+  }
+  if (ishtml) {
+    returnText += "</table>";
+  }
+  root.close();
+  foundfile.close();
+  return returnText;
+}
+// Make size of files human readable
+// source: https://github.com/CelliesProjects/minimalUploadAuthESP32
+String humanReadableSize(const size_t bytes) {
+  if (bytes < 1024) return String(bytes) + " B";
+  else if (bytes < (1024 * 1024)) return String(bytes / 1024.0) + " KB";
+  else if (bytes < (1024 * 1024 * 1024)) return String(bytes / 1024.0 / 1024.0) + " MB";
+  else return String(bytes / 1024.0 / 1024.0 / 1024.0) + " GB";
+}
+
+void configureWebServer() {
+  server->on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
+    String logmessage = "Client:" + request->client()->remoteIP().toString() + + " " + request->url();
+    Serial.println(logmessage);
+    request->send_P(200, "text/html", index_html, processor);
+  });
+
+  // run handleUpload function when any file is uploaded
+  server->on("/upload", HTTP_POST, [](AsyncWebServerRequest *request) {
+        request->send(200);
+      }, handleUpload);
+}
+
+// handles uploads
+void handleUpload(AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final) {
+  String logmessage = "Client:" + request->client()->remoteIP().toString() + " " + request->url();
+  Serial.println(logmessage);
+
+  if (!index) {
+    logmessage = "Upload Start: " + String(filename);
+    // open the file on first call and store the file handle in the request object
+    request->_tempFile = FFat.open("/" + filename, "w");
+    Serial.println(logmessage);
+  }
+
+  if (len) {
+    // stream the incoming chunk to the opened file
+    request->_tempFile.write(data, len);
+    logmessage = "Writing file: " + String(filename) + " index=" + String(index) + " len=" + String(len);
+    Serial.println(logmessage);
+  }
+
+  if (final) {
+    logmessage = "Upload Complete: " + String(filename) + ",size: " + String(index + len);
+    // close the file handle as the upload is now done
+    request->_tempFile.close();
+    Serial.println(logmessage);
+    request->redirect("/");
+  }
+}
+
+String processor(const String& var) {
+  if (var == "FILELIST") {
+    return listFilesRoot(true);
+  }
+  if (var == "FREESPIFFS") {
+    return humanReadableSize((FFat.totalBytes() - FFat.usedBytes()));
+  }
+
+  if (var == "USEDSPIFFS") {
+    return humanReadableSize(FFat.usedBytes());
+  }
+
+  if (var == "TOTALSPIFFS") {
+    return humanReadableSize(FFat.totalBytes());
+  }
+
+  return String();
+}
+
