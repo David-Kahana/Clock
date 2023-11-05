950ae6da8e24160b702693ecc7ebbbdc286fa0cf
David-Kahana <david.kahana@gmail.com>
2023-11-05 07:47:11 +0200

    for esp32s3

---
+++ ClockV02/scan_i2c/scan_i2c.ino
@@ -0,0 +1,41 @@
+#include <Wire.h>
+
+void scan_i2c() {
+  int8_t error, address;
+  int nDevices = 0;
+  for (address = 1; address < 127; address++) {
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+    if (error == 0) {
+      Serial.print("I2C device found at address 0x");
+      if (address < 16) {
+        Serial.print("0");
+      }
+      Serial.println(address, HEX);
+      nDevices++;
+    } else if (error == 4) {
+      Serial.print("Unknow error at address 0x");
+      if (address < 16) {
+        Serial.print("0");
+      }
+      Serial.println(address, HEX);
+    }
+  }
+  if (nDevices == 0) {
+    Serial.println("No I2C devices found\n");
+  } else {
+    Serial.println("done\n");
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin(8, 9);
+  delay(1000);
+}
+
+void loop() {
+  scan_i2c();
+  delay(4000);
+
+}
